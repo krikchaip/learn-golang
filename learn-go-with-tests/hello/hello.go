e96bd3ec979282c281018f7b10e1a996acd6f2d2
krikchaip <krikchai.p@gmail.com>
2022-07-21 18:05:01 +0700

    learn-go-with-tests: Hello supports Spanish

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return helloPrefix + name
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
