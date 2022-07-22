8c438ecb2a5f6024b774f8a7fed41244c671f14e
krikchaip <krikchai.p@gmail.com>
2022-07-23 02:23:42 +0700

    update 02-constants example

--- master-golang-programming/02-constants.go
+++ master-golang-programming/02-constants.go
@@ -69,4 +69,15 @@ func main() {
 		c22            // -> 2
 		c33            // -> 4
 	)
+
+	const (
+		_         = iota
+		Monday    // -> 1
+		Tuesday   // -> 2
+		Wednesday // -> 3
+		Thursday  // -> 4
+		Friday    // -> 5
+		Saturday  // -> 6
+		Sunday    // -> 7
+	)
 }
