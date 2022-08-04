28c395c63323543150140edbd15b8ebf241c4b57
krikchaip <krikchai.p@gmail.com>
2022-08-05 02:26:13 +0700

    learn-go-with-tests: part07 - implementation

--- learn-go-with-tests/07-maps/dictionary.go
+++ learn-go-with-tests/07-maps/dictionary.go
@@ -1,7 +1,18 @@
 package maps
 
+import (
+	"errors"
+)
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	// ?? map-lookup returns 2 values
+	val, exists := d[word]
+
+	if !exists {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return val, nil
 }
--- learn-go-with-tests/07-maps/dictionary_test.go
+++ learn-go-with-tests/07-maps/dictionary_test.go
@@ -6,12 +6,25 @@ func TestSearch(t *testing.T) {
 	// ?? map.key type must be conforms `comparable`
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	given := "test"
-	got := dictionary.Search(given)
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		given := "test"
+		got, _ := dictionary.Search(given)
+		want := "this is just a test"
 
-	assertStrings(t, got, want, given)
+		assertStrings(t, got, want, given)
+	})
 
+	t.Run("unknown word", func(t *testing.T) {
+		given := "unknown"
+		_, err := dictionary.Search(given)
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error but got none.")
+		}
+
+		assertStrings(t, err.Error(), want, given)
+	})
 }
 
 func assertStrings(t testing.TB, got, want, given string) {
