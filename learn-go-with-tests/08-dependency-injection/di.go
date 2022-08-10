6f4c33d90d4b6540b92fb8a0bfe8b62adf3dcdcf
krikchaip <krikchai.p@gmail.com>
2022-08-10 13:27:56 +0700

    learn-go-with-test: 08-dependency-injection refactor to general Writer

--- learn-go-with-tests/08-dependency-injection/di.go
+++ learn-go-with-tests/08-dependency-injection/di.go
@@ -1,10 +1,31 @@
 package dependency_injection
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+// ?? you are allow to pass a pointer of a type
+// ?? that implements the same interface as an interface parameter
+// ?? eg. *bytes.Buffer, *os.File, ...
+func Greet(writer io.Writer, name string) {
+	// ** this is actually what fmt.Printf does under the hood!
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func GreetStdout() {
+	Greet(os.Stdout, "Elodie\n")
+}
+
+func GreetHttp() {
+	// ** convert a normal function to HTTPHandler type
+	// ** this is basically like int(2.3), float(3), ...
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Greet(w, "world!") // ** w implementa Writer interface
+	})
+
+	// go to http://localhost:5001
+	http.ListenAndServe(":5001", handler)
 }
--- learn-go-with-tests/main/main.go
+++ learn-go-with-tests/main/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	hello "01-hello"
 	integers "02-integers"
+	di "08-dependency-injection" // name is too long :(
 	"fmt"
 )
 
 func main() {
 	fmt.Println("01-hello:", hello.Hello("Winner", hello.French))
 	fmt.Println("02-integers:", integers.Add(2, 5))
+	di.GreetStdout()
+	di.GreetHttp()
 }
