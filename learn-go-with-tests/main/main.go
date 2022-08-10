7acaed328cabe7d019460f0eee1ef1424a41dee7
krikchaip <krikchai.p@gmail.com>
2022-08-10 15:07:20 +0700

    learn-go-with-test: decouple 01-hello from main

--- learn-go-with-tests/main/main.go
+++ learn-go-with-tests/main/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	hello "01-hello"
 	integers "02-integers"
-
 	"fmt"
 )
 
 func main() {
-	fmt.Println("01-hello:", hello.Hello("Winner", hello.French))
 	fmt.Println("02-integers:", integers.Add(2, 5))
 }
