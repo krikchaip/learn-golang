674efcc79dc41e6a7e858be2ebb4ea3997076251
krikchaip <krikchai.p@gmail.com>
2022-08-10 16:37:24 +0700

    learn-go-with-test: 09-mocking complete the feature

--- learn-go-with-tests/09-mocking/lib/countdown.go
+++ learn-go-with-tests/09-mocking/lib/countdown.go
@@ -3,8 +3,17 @@ package countdown
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(writer io.Writer) {
-	fmt.Fprintf(writer, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Fprint(writer, finalWord)
 }
--- learn-go-with-tests/09-mocking/lib/countdown_test.go
+++ learn-go-with-tests/09-mocking/lib/countdown_test.go
@@ -11,7 +11,7 @@ func TestCountdown(t *testing.T) {
 	Countdown(buffer)
 
 	got := buffer.String()
-	want := "3"
+	want := "3\n2\n1\nGo!"
 
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
