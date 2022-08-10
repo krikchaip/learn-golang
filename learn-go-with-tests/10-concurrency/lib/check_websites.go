85900feb2c705089e97a4603d3f0f20034ba10d0
krikchaip <krikchai.p@gmail.com>
2022-08-11 00:49:55 +0700

    learn-go-with-test: 10-concurrency -> fixing concurrency issue

--- learn-go-with-tests/10-concurrency/lib/check_websites.go
+++ learn-go-with-tests/10-concurrency/lib/check_websites.go
@@ -3,11 +3,26 @@ package concurrency
 // returns whether the website returned a good or a bad response
 type WebsiteChecker func(url string) bool
 
+type result struct {
+	string // url
+	bool   // response status
+}
+
+// ?? run this program with `go test -race` to debug race condition
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		// ?? run this anonymous function in a separate process
+		go func(u string) {
+			resultChannel <- result{u, wc(u)} // ?? send statement
+		}(url)
+	}
+
+	for range urls {
+		r := <-resultChannel // ?? receive expression
+		results[r.string] = r.bool
 	}
 
 	return results
