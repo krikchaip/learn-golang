86a1280757cac019a222fe888a14b0bed2725f37
krikchaip <krikchai.p@gmail.com>
2022-08-13 02:20:21 +0700

    learn-go-with-test: 12-reflection test refactored

--- learn-go-with-tests/12-reflection/reflection.go
+++ learn-go-with-tests/12-reflection/reflection.go
@@ -3,7 +3,16 @@ package reflection
 import "reflect"
 
 func Walk(x any, fn func(string)) {
+	v1(x, fn)
+}
+
+func v1(x any, fn func(string)) {
+	// ?? get the value part of x (there are type, value, ...)
 	val := reflect.ValueOf(x)
+
+	// ?? assumes that `val` is a struct, get the first struct field value
 	field := val.Field(0)
+
+	// ?? assumes that `field` is a string
 	fn(field.String())
 }
--- learn-go-with-tests/12-reflection/reflection_test.go
+++ learn-go-with-tests/12-reflection/reflection_test.go
@@ -7,16 +7,26 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	x := struct{ Name string }{"Chris"}
+	cases := []struct {
+		Name   string
+		X      any
+		Result []string
+	}{
+		{Name: "simple", X: struct{ Name string }{"Chris"}, Result: []string{"Chris"}},
+	}
 
-	var got []string
-	want := []string{x.Name}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			want := test.Result
 
-	reflection.Walk(x, func(v string) {
-		got = append(got, v)
-	})
+			reflection.Walk(test.X, func(v string) {
+				got = append(got, v)
+			})
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
 	}
 }
