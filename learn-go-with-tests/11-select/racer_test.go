a5c28e268de2e03cb44f3a0fa0e02df4f82a6bdd
krikchaip <krikchai.p@gmail.com>
2022-08-12 14:43:09 +0700

    learn-go-with-test: 11-select more robust testing strategy

--- learn-go-with-tests/11-select/racer_test.go
+++ learn-go-with-tests/11-select/racer_test.go
@@ -2,15 +2,42 @@ package racer_test
 
 import (
 	racer "11-select"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestRacer(t *testing.T) {
+// ** [NOT RECOMMENDED] we're reaching out to real websites here !!
+func TestRacer_flaky(t *testing.T) {
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.dev"
 
 	got := racer.Racer(slowURL, fastURL)
 	want := fastURL
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+// ** [RECOMMENDED] using a mock HTTP server instead
+func TestRacer_robust(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer func() {
+		slowServer.Close()
+		fastServer.Close()
+	}()
+
+	got := racer.Racer(slowServer.URL, fastServer.URL)
+	want := fastServer.URL
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
