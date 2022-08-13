281174a4e5b193b765e69c43529dd1bb4f63d20a
krikchaip <krikchai.p@gmail.com>
2022-08-14 03:18:39 +0700

    master-golang-programming: url-downloader run downloader concurrently

--- master-golang-programming/url-downloader/main.go
+++ master-golang-programming/url-downloader/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"master-golang-programming/url-downloader/lib"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -11,7 +14,33 @@ func main() {
 		"https://www.medium.com",
 	}
 
-	for _, url := range urls {
-		lib.DownloadURL(url)
+	measureTime(func() {
+		// for _, url := range urls {
+		// 	lib.DownloadURL(url)
+		// }
+
+		concurrently(urls, lib.DownloadURL)
+	})
+}
+
+func concurrently[T, U any](tasks []T, fn func(T) U) {
+	var wg sync.WaitGroup
+
+	wg.Add(len(tasks))
+
+	for _, t := range tasks {
+		go func(t T) {
+			fn(t)
+			wg.Done()
+		}(t)
 	}
+
+	wg.Wait()
+}
+
+func measureTime(f func()) {
+	start := time.Now()
+	f()
+	used := time.Since(start)
+	fmt.Println("time used:", used.Seconds(), "seconds")
 }
