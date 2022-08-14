fe131b7d84321bb57a1cd78d010663dd0b29ee65
krikchaip <krikchai.p@gmail.com>
2022-08-15 03:26:11 +0700

    master-golang-programming: 22-channels finished

--- master-golang-programming/22-channels/main.go
+++ master-golang-programming/22-channels/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// ?? declaring a bidirectional channel
@@ -8,26 +11,52 @@ func main() {
 
 	fmt.Println(ch1) // 0x... (channel is a pointer)
 
-	// ?? Declaring and initilizing a RECEIVE-ONLY channel (unidirectional)
+	// ?? declaring and initilizing a RECEIVE-ONLY channel (unidirectional)
 	ch2 := make(<-chan string)
 
-	// ?? Declaring and initilizing a SEND-ONLY channel (unidirectional)
+	// ?? declaring and initilizing a SEND-ONLY channel (unidirectional)
 	ch3 := make(chan<- string)
 
 	fmt.Printf("%T, %T, %T\n", ch1, ch2, ch3) // chan int, <-chan string, chan<- string
 
-	// ** calling these statements alone inside main goroutine is not allowed!!
+	// ** using "send statement" inside the main goroutine will cause a DEADLOCK
 	// ch1 <- 10
+
+	// ** as opposed to the "receive expression" that is allowed
+	// ** as long as there's some content inside
 	// num := <-ch1
-	// _ = num
 
-	// ** must call inside a goroutine !!
+	// ** must call this inside a goroutine !!
 	go putN(10, ch1)
-	n := <-ch1
 
-	fmt.Println("Value received:", n) // 10
+	fmt.Println("Value received:", <-ch1) // 10
+
+	ball := make(chan string)
+
+	// ?? sending a message to "ball"
+	go ping(ball)
+
+	// ?? this is like calling "<-ball" continuously
+	for res := range ball {
+		fmt.Println(res)
+		time.Sleep(1 * time.Second)
+		switch res {
+		case "PING":
+			go pong(ball)
+		case "PONG":
+			go ping(ball)
+		}
+	}
 }
 
 func putN(n int, c chan int) {
 	c <- n
+}
+
+func ping(c chan<- string) {
+	c <- "PING"
+}
+
+func pong(c chan string) {
+	c <- "PONG"
 }
