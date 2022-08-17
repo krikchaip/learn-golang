d552f63df90539e6830cd625043af759c68d86b9
krikchaip <krikchai.p@gmail.com>
2022-08-17 18:55:40 +0700

    learn-go-with-tests: 15-property-based-tests supports up to 39

--- learn-go-with-tests/15-property-based-tests/numeral.go
+++ learn-go-with-tests/15-property-based-tests/numeral.go
@@ -1,11 +1,34 @@
 package numeral
 
+import (
+	"strings"
+)
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+	// ?? A Builder is used to efficiently build a string using Write methods.
+	// ?? It minimizes memory copying.
+	var result strings.Builder // implements: io.Writer
+
+	// ?? more efficient than string literal concatenation
+	for arabic > 0 {
+		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	if arabic == 3 {
-		return "III"
-	}
-	return "I"
+
+	return result.String()
 }
--- learn-go-with-tests/15-property-based-tests/numeral_test.go
+++ learn-go-with-tests/15-property-based-tests/numeral_test.go
@@ -14,6 +14,16 @@ func TestRomanNumerals(t *testing.T) {
 		{name: "1->I", input: 1, output: "I"},
 		{name: "2->II", input: 2, output: "II"},
 		{name: "3->III", input: 3, output: "III"},
+		{name: "4->IV", input: 4, output: "IV"},
+		{name: "5->V", input: 5, output: "V"},
+		{name: "6->VI", input: 6, output: "VI"},
+		{name: "7->VI", input: 7, output: "VII"},
+		{name: "9->IX", input: 9, output: "IX"},
+		{name: "10->X", input: 10, output: "X"},
+		{name: "14->XIV", input: 14, output: "XIV"},
+		{name: "18->XVIII", input: 18, output: "XVIII"},
+		{name: "20->XX", input: 20, output: "XX"},
+		{name: "39->XXXIX", input: 39, output: "XXXIX"},
 	}
 
 	for _, test := range table {
