e1a75f295216e5cbda9c139992bed2bca396b129
krikchaip <krikchai.p@gmail.com>
2022-08-17 19:05:19 +0700

    learn-go-with-tests: 15-property-based-tests logic/test refactoring

--- learn-go-with-tests/15-property-based-tests/numeral.go
+++ learn-go-with-tests/15-property-based-tests/numeral.go
@@ -4,29 +4,29 @@ import (
 	"strings"
 )
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	// ?? A Builder is used to efficiently build a string using Write methods.
 	// ?? It minimizes memory copying.
 	var result strings.Builder // implements: io.Writer
 
 	// ?? more efficient than string literal concatenation
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
--- learn-go-with-tests/15-property-based-tests/numeral_test.go
+++ learn-go-with-tests/15-property-based-tests/numeral_test.go
@@ -2,34 +2,35 @@ package numeral_test
 
 import (
 	numeral "15-property-based-tests"
+	"fmt"
 	"testing"
 )
 
 func TestRomanNumerals(t *testing.T) {
 	table := []struct {
-		name   string
-		input  int
-		output string
+		Arabic int
+		Roman  string
 	}{
-		{name: "1->I", input: 1, output: "I"},
-		{name: "2->II", input: 2, output: "II"},
-		{name: "3->III", input: 3, output: "III"},
-		{name: "4->IV", input: 4, output: "IV"},
-		{name: "5->V", input: 5, output: "V"},
-		{name: "6->VI", input: 6, output: "VI"},
-		{name: "7->VI", input: 7, output: "VII"},
-		{name: "9->IX", input: 9, output: "IX"},
-		{name: "10->X", input: 10, output: "X"},
-		{name: "14->XIV", input: 14, output: "XIV"},
-		{name: "18->XVIII", input: 18, output: "XVIII"},
-		{name: "20->XX", input: 20, output: "XX"},
-		{name: "39->XXXIX", input: 39, output: "XXXIX"},
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
 	}
 
 	for _, test := range table {
-		t.Run(test.name, func(t *testing.T) {
-			got := numeral.ConvertToRoman(test.input)
-			want := test.output
+		name := fmt.Sprintf("%d->%s", test.Arabic, test.Roman)
+		t.Run(name, func(t *testing.T) {
+			got := numeral.ConvertToRoman(test.Arabic)
+			want := test.Roman
 
 			if got != want {
 				t.Errorf("got %q, want %q", got, want)
