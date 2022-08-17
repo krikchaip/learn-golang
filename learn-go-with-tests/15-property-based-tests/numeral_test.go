cee683f898120fefdbe248ce6955c6d7ebc5be92
krikchaip <krikchai.p@gmail.com>
2022-08-17 19:15:28 +0700

    learn-go-with-tests: 15-property-based-tests finished convertToRoman

--- learn-go-with-tests/15-property-based-tests/numeral_test.go
+++ learn-go-with-tests/15-property-based-tests/numeral_test.go
@@ -24,6 +24,23 @@ func TestRomanNumerals(t *testing.T) {
 		{Arabic: 18, Roman: "XVIII"},
 		{Arabic: 20, Roman: "XX"},
 		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 1996, Roman: "MCMXCVI"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 2539, Roman: "MMDXXXIX"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
 	}
 
 	for _, test := range table {
