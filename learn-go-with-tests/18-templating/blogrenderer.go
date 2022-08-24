47777746981957568a2b6ab5c6f632877efed8f0
krikchaip <krikchai.p@gmail.com>
2022-08-25 00:35:41 +0700

    learn-go-with-tests: 18-templating write the code just to pass the test

--- learn-go-with-tests/18-templating/blogrenderer.go
+++ learn-go-with-tests/18-templating/blogrenderer.go
@@ -4,13 +4,36 @@ import (
 	blogposts "17-reading-files/lib"
 	"fmt"
 	"io"
+	"strings"
 )
 
 func Render(w io.Writer, post blogposts.Post) error {
-	_, err := fmt.Fprint(w, tag("h1", post.Title))
+	_, err := fmt.Fprint(w,
+		tag("h1", post.Title),
+		tag("p", post.Description),
+		fmt.Sprintf(
+			"Tags: %s",
+			tag("ul",
+				strings.Join(transform(post.Tags, func(t string) string {
+					return tag("li", t)
+				}), ""),
+			),
+		),
+	)
+
 	return err
 }
 
 func tag(name, content string) string {
 	return fmt.Sprintf("<%[1]s>%[2]s</%[1]s>", name, content)
+}
+
+func transform[T, U any](xs []T, f func(x T) U) []U {
+	results := make([]U, 0, len(xs))
+
+	for _, x := range xs {
+		results = append(results, f(x))
+	}
+
+	return results
 }
--- learn-go-with-tests/18-templating/blogrenderer_test.go
+++ learn-go-with-tests/18-templating/blogrenderer_test.go
@@ -26,7 +26,7 @@ func TestRender(t *testing.T) {
 		}
 
 		got := w.String()
-		want := `<h1>hello world</h1>`
+		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
