a73b7963b029d9ac023f4981374a9accade9c76c
krikchaip <krikchai.p@gmail.com>
2022-09-02 03:39:33 +0700

    learn-go-with-tests: 21-acceptance-testing FindRoot util

--- learn-go-with-tests/21-acceptance-testing/lib/util/utils.go
+++ learn-go-with-tests/21-acceptance-testing/lib/util/utils.go
@@ -1,11 +1,39 @@
 package util
 
 import (
+	"fmt"
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func Catch() {
 	if err := recover(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func FindRoot(start string) (packageRoot string, err error) {
+	var entries []fs.DirEntry
+
+	if start == filepath.FromSlash("/") {
+		return "", fmt.Errorf("go.mod was not found along the path\n")
+	}
+
+	if !filepath.IsAbs(start) {
+		start, _ = filepath.Abs(start)
+	}
+
+	if entries, err = os.ReadDir(start); err != nil {
+		return "", err
+	}
+
+	for _, ent := range entries {
+		if ent.Name() == "go.mod" && ent.Type().IsRegular() {
+			return start, nil
+		}
+	}
+
+	return FindRoot(filepath.Dir(start))
 }
