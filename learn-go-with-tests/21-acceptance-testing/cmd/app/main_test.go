21f5ff870cb9911abd40777477b1ca675df4fcbb
krikchaip <krikchai.p@gmail.com>
2022-09-03 02:50:37 +0700

    learn-go-with-tests: 21-acceptance-testing gracefulshutdown test scaffold

--- learn-go-with-tests/21-acceptance-testing/cmd/app/main_test.go
+++ learn-go-with-tests/21-acceptance-testing/cmd/app/main_test.go
@@ -2,20 +2,32 @@ package main_test
 
 import (
 	"testing"
+	"time"
 
 	"21-acceptance-testing/lib/testutil"
 	"21-acceptance-testing/lib/testutil/assert"
 )
 
 const (
-	port = "8080"
-	url  = "http://localhost:" + port
+	PORT string = "8080"
+	URL  string = "http://localhost:" + PORT
 )
 
 func TestGracefulShutdown(t *testing.T) {
-	cleanup, _ := testutil.LaunchTestProgram(port)
+	cleanup, interupt := testutil.LaunchTestProgram(PORT)
 	t.Cleanup(cleanup) // ?? afterThis()
 
 	// just check the server works before we shut things down
-	assert.CanGet(t, url)
+	assert.CanGet(t, URL)
+
+	// fire off a request, and before it has a chance to respond send SIGTERM.
+	time.AfterFunc(50*time.Millisecond, func() {
+		assert.NoPanic(t, interupt)
+	})
+
+	// Without graceful shutdown, this would fail
+	assert.CanGet(t, URL)
+
+	// // after interrupt, the server should be shutdown, and no more requests will work
+	// assert.CantGet(t, URL)
 }
---
+++ learn-go-with-tests/21-acceptance-testing/lib/gracefulshutdown/gracefulshutdown_test.go
@@ -0,0 +1,74 @@
+package gracefulshutdown_test
+
+import (
+	"os"
+	"testing"
+
+	"21-acceptance-testing/lib/gracefulshutdown"
+	"21-acceptance-testing/lib/testutil/assert"
+)
+
+func TestGracefulShutdownServer_ListenAndServe(t *testing.T) {
+	t.Run("wait for interupt, shutdown gracefully", func(t *testing.T) {
+		interrupt := make(chan os.Signal, 1)
+		spyServer := newSpyServer()
+		server := gracefulshutdown.NewServer(spyServer)
+
+		spyServer.listenAndServeFn = func() error {
+			return nil
+		}
+
+		spyServer.shutdownFn = func() error {
+			return nil
+		}
+
+		go func() {
+			if err := server.ListenAndServe(); err != nil {
+				t.Error(err)
+			}
+		}()
+
+		// verify we call listen on the delegate server
+		spyServer.assertListen(t)
+
+		// verify we call shutdown on the delegate server when an interrupt is made
+		interrupt <- os.Interrupt
+		spyServer.assertShutdown(t)
+	})
+}
+
+// implements: gracefulshutdown.HttpServer
+type spyServer struct {
+	listenAndServeFn func() error
+	shutdownFn       func() error
+
+	listened chan struct{}
+	shutdown chan struct{}
+}
+
+func newSpyServer() *spyServer {
+	return &spyServer{
+		listened: make(chan struct{}, 1),
+		shutdown: make(chan struct{}, 1),
+	}
+}
+
+func (s *spyServer) ListenAndServe() error {
+	s.listened <- struct{}{}
+	return s.listenAndServeFn()
+}
+
+func (s *spyServer) Shutdown() error {
+	s.shutdown <- struct{}{}
+	return s.shutdownFn()
+}
+
+func (s *spyServer) assertListen(t testing.TB) {
+	t.Helper()
+	assert.SignalSent(t, s.listened, "listened")
+}
+
+func (s *spyServer) assertShutdown(t testing.TB) {
+	t.Helper()
+	assert.SignalSent(t, s.shutdown, "shutdown")
+}
