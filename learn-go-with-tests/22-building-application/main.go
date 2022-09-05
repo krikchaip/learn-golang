7189d94e70cbebcdc78f45d07e54c2a1062e2215
krikchaip <krikchai.p@gmail.com>
2022-09-06 03:51:38 +0700

    learn-go-with-tests: 22-building-application fixed main.go file

--- learn-go-with-tests/22-building-application/main.go
+++ learn-go-with-tests/22-building-application/main.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main() {
-	sv := &server.PlayerServer{}
+	sv := server.NewPlayerServer(&InMemoryPlayerStore{})
 
 	// we wrap the call in log.Fatal
 	// just in case if there is a problem with ListenAndServe.
 	// eg. port already being used, etc.
 	log.Fatal(http.ListenAndServe(":3000", sv))
+}
+
+// TODO: will implement later
+type InMemoryPlayerStore struct{}
+
+func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 0
 }
