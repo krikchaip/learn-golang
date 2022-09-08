85541a533bfc0d0f69200e3ce4d6e66ba46c83bd
krikchaip <krikchai.p@gmail.com>
2022-09-08 21:37:50 +0700

    learn-go-with-tests: 22-building-application setup application

--- learn-go-with-tests/22-building-application/main.go
+++ learn-go-with-tests/22-building-application/main.go
@@ -3,17 +3,53 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
 
+	"21-acceptance-testing/lib/util"
+	"22-building-application/entity"
 	"22-building-application/server"
 	"22-building-application/store"
 )
 
+const DB_SOURCE = "assets/game.db.json"
+
+// go run 22-building-application OR
+// cd 22-building-application && go run .
 func main() {
-	st := store.NewInMemoryPlayerStore()
+	st := setupStore()
 	sv := server.NewPlayerServer(st)
 
 	// we wrap the call in log.Fatal
 	// just in case if there is a problem with ListenAndServe.
 	// eg. port already being used, etc.
 	log.Fatal(http.ListenAndServe(":3000", sv))
+}
+
+func setupStore() entity.PlayerStore {
+	// store := store.NewInMemoryPlayerStore()
+
+	sourceName := resolvePath(DB_SOURCE)
+	perm := os.O_CREATE | os.O_RDWR
+	source, err := os.OpenFile(sourceName, perm, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", sourceName, err)
+	}
+
+	store := store.NewFileSystemPlayerStore(source)
+
+	return store
+}
+
+func resolvePath(rel string) string {
+	_, filename, _, _ := runtime.Caller(0)
+	pkgRoot, err := util.FindRoot(filepath.Dir(filename))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return filepath.Join(pkgRoot, rel)
 }
--- learn-go-with-tests/22-building-application/store/filesystem.go
+++ learn-go-with-tests/22-building-application/store/filesystem.go
@@ -10,15 +10,24 @@ import (
 // implements: server.PlayerStore
 type FileSystemPlayerStore struct {
 	source io.ReadWriteSeeker
+	cache  entity.League
 	mut    sync.Mutex
 }
 
 func NewFileSystemPlayerStore(source io.ReadWriteSeeker) entity.PlayerStore {
-	return &FileSystemPlayerStore{source: source}
+	source.Seek(0, io.SeekStart)
+
+	// initialize cache to improve performance
+	cache, _ := entity.NewLeague(source)
+
+	return &FileSystemPlayerStore{
+		source: source,
+		cache:  cache,
+	}
 }
 
 func (s *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	player := s.GetLeagueTable().Find(name)
+	player := s.cache.Find(name)
 
 	if player == nil {
 		return 0
@@ -28,8 +37,7 @@ func (s *FileSystemPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (s *FileSystemPlayerStore) GetLeagueTable() (league entity.League) {
-	s.source.Seek(0, io.SeekStart)
-	league, _ = entity.NewLeague(s.source)
+	league = s.cache
 	return
 }
 
@@ -37,18 +45,17 @@ func (s *FileSystemPlayerStore) RecordWin(name string) {
 	s.mut.Lock()
 	defer s.mut.Unlock()
 
-	league := s.GetLeagueTable()
-	player := league.Find(name)
+	player := s.cache.Find(name)
 
 	if player != nil {
 		player.Wins++
 	} else {
-		league = append(league, entity.Player{Name: name, Wins: 1})
+		s.cache = append(s.cache, entity.Player{Name: name, Wins: 1})
 	}
 
 	// because the file cursor has already reached the end
 	// from calling s.GetLeagueTable()
 	s.source.Seek(0, io.SeekStart)
 
-	json.NewEncoder(s.source).Encode(league)
+	json.NewEncoder(s.source).Encode(s.cache)
 }
