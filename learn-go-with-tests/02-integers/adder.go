028dd550c585237edd15eeaa44564ea634c76d5d
krikchaip <krikchai.p@gmail.com>
2024-12-16 03:22:53 +0700

    learn-go-with-tests: refactor 02-adder

--- learn-go-with-tests/02-integers/adder.go
+++ learn-go-with-tests/02-integers/adder.go
@@ -1,4 +1,10 @@
-package integers
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("02-integers:", Add(2, 5))
+}
 
 // Add takes two integers and returns the sum of them.
 func Add(x, y int) int {
