8358cca153b46cdcfca09c420a6f628602215d32
krikchaip <krikchai.p@gmail.com>
2024-12-26 18:09:52 +0700

    learn-go-with-tests: fix 16-maths file loading

--- learn-go-with-tests/16-maths/main.go
+++ learn-go-with-tests/16-maths/main.go
@@ -7,8 +7,7 @@ import (
 	clockface "16-maths/lib"
 )
 
-// go run 16-maths/main.go
 func main() {
 	clock := clockface.ClockSVG(time.Now())
-	os.WriteFile("16-maths/assets/clock.svg", []byte(clock), 0666)
+	os.WriteFile("assets/clock.svg", []byte(clock), 0666)
 }
