e5da7893c1f1103f3d4550d202ee869801bb782d
krikchaip <krikchai.p@gmail.com>
2025-01-02 00:09:13 +0700

    snippetbox: postgres database setup

---
+++ lets-go/snippetbox/db/migrations/01-create-snippets.sql
@@ -0,0 +1,9 @@
+CREATE TABlE snippets (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW(),
+  expires TIMESTAMP NOT NULL
+);
+
+CREATE INDEX idx_snippets_created ON snippets(created);
---
+++ lets-go/snippetbox/db/migrations/02-seed-snippets.sql
@@ -0,0 +1,17 @@
+INSERT INTO snippets (title, content, expires) VALUES (
+  'An old silent pond',
+  'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō',
+  NOW() + INTERVAL '1 year'
+);
+
+INSERT INTO snippets (title, content, expires) VALUES (
+  'Over the wintry forest',
+  'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki',
+  NOW() + INTERVAL '1 year'
+);
+
+INSERT INTO snippets (title, content, expires) VALUES (
+  'First autumn morning',
+  'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo',
+  NOW() + INTERVAL '7 days'
+);
