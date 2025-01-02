37881cf11fd8332e71855e75dbcc067b3d1b792e
krikchaip <krikchai.p@gmail.com>
2025-01-02 16:32:32 +0700

    snippetbox: grant sequence access to 'web'

--- lets-go/snippetbox/db/migrations/03-add-role-web.sql
+++ lets-go/snippetbox/db/migrations/03-add-role-web.sql
@@ -4,3 +4,10 @@ GRANT INSERT, SELECT, UPDATE, DELETE
 ON ALL TABLES
 IN SCHEMA "public"
 TO web;
+
+-- the user also requires permissions for SEQUENCE data type
+-- ref: https://stackoverflow.com/questions/9325017/error-permission-denied-for-sequence-cities-id-seq-using-postgres
+GRANT SELECT, USAGE
+ON ALL SEQUENCES
+IN SCHEMA "public"
+TO web;
