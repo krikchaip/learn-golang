5fa3775cbdacda9a56fb460c310b0961273a4c9f
krikchaip <krikchai.p@gmail.com>
2025-01-02 17:18:16 +0700

    snippetbox: snippet.Insert implementation

--- lets-go/snippetbox/internal/models/snippets.go
+++ lets-go/snippetbox/internal/models/snippets.go
@@ -24,8 +24,27 @@ func NewSnippetModel(db *sql.DB) *SnippetModel {
 }
 
 // This will insert a new snippet into the database.
-func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
-	return 0, nil
+func (m *SnippetModel) Insert(title, content string, expires int) (id int, err error) {
+	// postgresql does not support Exec().LastInsertId() so we have to find another way
+	// ref: https://stackoverflow.com/questions/33382981/go-how-to-get-last-insert-id-on-postgresql-with-namedexec
+	//      https://stackoverflow.com/questions/71378392/go-postgres-prepared-statement-with-interval-parameter-not-working
+	row := m.db.QueryRow(`
+		INSERT INTO snippets (title, content, expires)
+		VALUES ($1, $2, NOW() + INTERVAL '1 day' * $3)
+		RETURNING id
+	`, title, content, expires)
+
+	err = row.Err()
+	if err != nil {
+		return 0, err
+	}
+
+	// load the result into the 'id' variable
+	if err = row.Scan(&id); err != nil {
+		return 0, err
+	}
+
+	return
 }
 
 // This will return a specific snippet based on its id.
