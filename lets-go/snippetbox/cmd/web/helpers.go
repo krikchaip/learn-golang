8851f6c20d83ba2fca47d012d8b1ac9c442561ca
krikchaip <krikchai.p@gmail.com>
2025-01-03 19:56:19 +0700

    snippetbox: template render util

--- lets-go/snippetbox/cmd/web/helpers.go
+++ lets-go/snippetbox/cmd/web/helpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
@@ -14,4 +17,30 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 
 func (app *application) cilentError(w http.ResponseWriter, statusCode int) {
 	http.Error(w, http.StatusText(statusCode), statusCode)
+}
+
+func (app *application) render(
+	w http.ResponseWriter,
+	r *http.Request,
+	status int,
+	page string,
+	data templateData,
+) {
+	// retrieve the appropriate template set from the cache
+	// based on the page name (like 'home' page)
+	t, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %q does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	// Write out the provided HTTP status code ('200 OK', '400 Bad Request' etc).
+	// NOTE: MUST BE executed before any call to Write()
+	w.WriteHeader(status)
+
+	if err := t.ExecuteTemplate(w, "base", data); err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 }
