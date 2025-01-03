1818675a341b5acb23e92dddfd3ffe78f8f25b54
krikchaip <krikchai.p@gmail.com>
2025-01-04 04:22:41 +0700

    snippetbox: wrap app.routes() with securityHeaders middleware

--- lets-go/snippetbox/cmd/web/routes.go
+++ lets-go/snippetbox/cmd/web/routes.go
@@ -2,11 +2,19 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	router := http.NewServeMux()
 
 	// serve files out of the "./ui/static" directory
 	fileServer := http.FileServer(http.Dir("ui/static"))
+
+	// serves static files (subtree path pattern)
+	// will match "/static/**", eg. "/static/css/main.css"
+	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// a catch-all handler (subtree path pattern)
+	// will match "/**", eg. "/foo", "/bar/bax/..."
+	// router.HandleFunc("/", app.defaultHandler)
 
 	// match a single slash, followed by nothing else (exact match)
 	router.HandleFunc("GET /{$}", app.home)
@@ -17,13 +25,7 @@ func (app *application) routes() *http.ServeMux {
 	router.HandleFunc("GET /snippet/create", app.snippetCreate)
 	router.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	// serves static files (subtree path pattern)
-	// will match "/static/**", eg. "/static/css/main.css"
-	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// a catch-all handler (subtree path pattern)
-	// will match "/**", eg. "/foo", "/bar/bax/..."
-	// router.HandleFunc("/", app.defaultHandler)
-
-	return router
+	// wrap ServeMux router with the 'securityHeaders' middleware.
+	// do note that ServeMux also implements the 'http.Handler' interface
+	return securityHeaders(router)
 }
