be36bd73668ea2e91889afd3fcf23dd9600b6563
krikchaip <krikchai.p@gmail.com>
2025-01-07 05:05:15 +0700

    snippetbox: snippetCreatePost accept params

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -63,9 +63,22 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	// parse form data received from the client.
+	// the parsed data will be put into the r.PostForm and r.Form struct
+	if err := r.ParseForm(); err != nil {
+		app.cilentError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// parse "expires" into an interger before using
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.cilentError(w, http.StatusBadRequest)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
