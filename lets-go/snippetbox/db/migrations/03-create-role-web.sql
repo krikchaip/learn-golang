01ad6ed1516d45cb8e8bf0af19f965506134270e
krikchaip <krikchai.p@gmail.com>
2025-01-08 04:48:22 +0700

    snippetbox: fix 'web' db user permissions for future objects

--- lets-go/snippetbox/db/migrations/03-create-role-web.sql
+++ lets-go/snippetbox/db/migrations/03-create-role-web.sql
@@ -11,3 +11,18 @@ GRANT SELECT, USAGE
 ON ALL SEQUENCES
 IN SCHEMA "public"
 TO web;
+
+-- granting permissions for future created postgres objects (tables, sequences, ...)
+-- refs: https://stackoverflow.com/questions/76952543/postgres-granting-all-permissions-but-still-denied-on-tables
+--       https://www.postgresql.org/docs/current/sql-alterdefaultprivileges.html
+ALTER DEFAULT PRIVILEGES
+IN SCHEMA "public"
+GRANT INSERT, SELECT, UPDATE, DELETE
+ON TABLES
+TO web;
+
+ALTER DEFAULT PRIVILEGES
+IN SCHEMA "public"
+GRANT SELECT, USAGE
+ON SEQUENCES
+TO web;
