5f8576bf9c6922177e772da897dcd246275ff3c8
krikchaip <krikchai.p@gmail.com>
2025-01-09 23:20:22 +0700

    snippetbox: (migration) create users table

---
+++ lets-go/snippetbox/db/migrations/05-create-users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  hashed_password CHAR(60) NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW()
+);
