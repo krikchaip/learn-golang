b0079be6612d3c71a52c469abff671e7bfb15f8b
krikchaip <krikchai.p@gmail.com>
2025-01-11 13:42:57 +0700

    snippetbox: embed FS to ui/static folder

---
+++ lets-go/snippetbox/ui/efs.go
@@ -0,0 +1,13 @@
+package ui
+
+import "embed"
+
+// embedding files/folders into package variables
+// NOTE:
+//   - the path must be relative to this .go file
+//   - can only be use on global variables at package level
+//   - paths can not contain . or .. elements
+//   - the embed FS is _always_ rooted in the directory containing the .go file
+
+//go:embed "static"
+var Files embed.FS
