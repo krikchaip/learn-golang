692a35c59fbe5515a8986adb49eb3e8f4c0dff18
krikchaip <krikchai.p@gmail.com>
2025-01-13 23:58:20 +0700

    snippetbox: add tests for snippetView handler

--- lets-go/snippetbox/cmd/web/main_test.go
+++ lets-go/snippetbox/cmd/web/main_test.go
@@ -28,6 +28,65 @@ func TestHealthzE2E(t *testing.T) {
 	assert.Equal(t, body, "OK")
 }
 
+func TestSnippetViewE2E(t *testing.T) {
+	app := newTestApplication(t)
+	server := testutils.NewTestServer(t, app.routes())
+
+	// must call Close() so that the server is shutdown when the test finishes
+	defer server.Close()
+
+	cases := []struct {
+		name     string
+		path     string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			path:     "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond...",
+		},
+		{
+			name:     "Non-existent ID",
+			path:     "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			path:     "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal ID",
+			path:     "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			path:     "/snippet/view/foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			path:     "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			statusCode, _, body := server.Get(t, c.path)
+
+			assert.Equal(t, statusCode, c.wantCode)
+
+			if c.wantBody != "" {
+				assert.StringContains(t, body, c.wantBody)
+			}
+		})
+	}
+}
+
 func newTestApplication(t *testing.T) *application {
 	// will discard anything written to io.Discard
 	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
