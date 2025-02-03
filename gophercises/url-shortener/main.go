9c87bfccf7481ffb19299c431a0b7b3a788d5c8b
krikchaip <krikchai.p@gmail.com>
2025-02-04 01:26:51 +0700

    url-shortener: middlewares scaffold

--- gophercises/url-shortener/main.go
+++ gophercises/url-shortener/main.go
@@ -8,9 +8,29 @@ import (
 const ADDR = ":8000"
 
 func main() {
-	URLShortener := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "OK")
-	})
+	URLShortener := YAMLHandler(nil)(MapHandler(nil)(FallbackHandler))
 
 	http.ListenAndServe(ADDR, URLShortener)
+}
+
+var FallbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "OK")
+})
+
+func MapHandler(urlPaths map[string]string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("hello from MapHandler")
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func YAMLHandler(yaml []byte) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("hello from YAMLHandler")
+			next.ServeHTTP(w, r)
+		})
+	}
 }
