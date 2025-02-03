5527fa994b1394e977aca540db9c4b76a306613b
krikchaip <krikchai.p@gmail.com>
2025-02-04 02:59:55 +0700

    url-shortener: implement YAMLHandler middleware

--- gophercises/url-shortener/go.mod
+++ gophercises/url-shortener/go.mod
@@ -1,3 +1,5 @@
 module krikchaip/url-shortener
 
 go 1.23.4
+
+require gopkg.in/yaml.v3 v3.0.1
--- gophercises/url-shortener/main.go
+++ gophercises/url-shortener/main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"slices"
+
+	"gopkg.in/yaml.v3"
 )
 
 const ADDR = ":8000"
 
 func main() {
-	URLShortener := MapHandler(map[string]string{
+	URLShortener := YAMLHandler([]byte(`
+    - path: /urlshort
+      url: https://github.com/gophercises/urlshort
+    - path: /urlshort-final
+      url: https://github.com/gophercises/urlshort/tree/solution
+  `)).then(MapHandler(map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 		"/github":         "https://github.com/krikchaip",
-	})
+	}))
 
 	http.ListenAndServe(ADDR, URLShortener(FallbackHandler))
 }
@@ -37,10 +45,33 @@ func MapHandler(urlPaths map[string]string) Middleware {
 	}
 }
 
-func YAMLHandler(yaml []byte) Middleware {
+type URLPathYAML struct {
+	Path string
+	Url  string
+}
+
+func YAMLHandler(blob []byte) Middleware {
+	var urlPaths []URLPathYAML
+
+	if err := yaml.Unmarshal(blob, &urlPaths); err != nil {
+		log.Fatal(err)
+	}
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("hello from YAMLHandler")
+			i := slices.IndexFunc(urlPaths, func(rec URLPathYAML) bool {
+				return rec.Path == r.URL.Path
+			})
+
+			if i != -1 {
+				url := urlPaths[i].Url
+
+				log.Println(r.URL.Path, "->", url)
+				http.Redirect(w, r, url, http.StatusMovedPermanently)
+
+				return
+			}
+
 			next.ServeHTTP(w, r)
 		})
 	}
