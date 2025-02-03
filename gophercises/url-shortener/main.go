d5a1a316d6db280e65495a422298649d5d4b0bca
krikchaip <krikchai.p@gmail.com>
2025-02-04 02:12:53 +0700

    url-shortener: implement MapHandler middleware

--- gophercises/url-shortener/main.go
+++ gophercises/url-shortener/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 const ADDR = ":8000"
 
 func main() {
-	URLShortener := MapHandler(nil)
+	URLShortener := MapHandler(map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+		"/github":         "https://github.com/krikchaip",
+	})
 
 	http.ListenAndServe(ADDR, URLShortener(FallbackHandler))
 }
@@ -20,7 +25,13 @@ var FallbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reque
 func MapHandler(urlPaths map[string]string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("hello from MapHandler")
+			if url, ok := urlPaths[r.URL.Path]; ok {
+				log.Println(r.URL.Path, "->", url)
+				http.Redirect(w, r, url, http.StatusMovedPermanently)
+
+				return
+			}
+
 			next.ServeHTTP(w, r)
 		})
 	}
