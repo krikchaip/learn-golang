fd662fb8e8f6ec7a56c731ebf489d698090ba62f
krikchaip <krikchai.p@gmail.com>
2025-03-25 21:10:15 +0700

    choose-your-own-adventure: model for stories

---
+++ gophercises/choose-your-own-adventure/internal/model/stories.go
@@ -0,0 +1,12 @@
+package model
+
+type Stories map[string]struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
