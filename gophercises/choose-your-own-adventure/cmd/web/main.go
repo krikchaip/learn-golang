b2533af4dc6d4398aa80256a2ee6c37503e86f68
krikchaip <krikchai.p@gmail.com>
2025-03-25 23:41:22 +0700

    choose-your-own-adventure: finished

--- gophercises/choose-your-own-adventure/cmd/web/main.go
+++ gophercises/choose-your-own-adventure/cmd/web/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"krikchaip/choose-your-own-adventure/pages"
 	"net/http"
 )
 
 func main() {
 	stories := loadJSON(args.Filepath)
+	t := loadTemplate(pages.FS)
 
 	http.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, args.Root, http.StatusSeeOther)
 	})
 
 	http.HandleFunc("/{arc}", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, toJSON(stories[r.PathValue("arc")]))
+		arc := r.PathValue("arc")
+		s, ok := stories[arc]
+
+		if !ok {
+			http.NotFound(w, r)
+			return
+		}
+
+		t.Execute(w, s)
 	})
 
 	serveHTTP(args.Addr)
